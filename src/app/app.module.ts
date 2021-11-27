3483b4e108345578a745963899e5a0d63d57408f
NAP-BN-Test <caonv174@gmail.com>
2021-11-27 11:53:23 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ToDoItemComponent } from './components/to-do-item/to-do-item.component';
+import { FormTaskComponent } from './components/form-task/form-task.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ToDoItemComponent, FormTaskComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
